f26b7578f0239eabb49d04844fb1060f92852742
KeisukeKawamura <kawamura@nanase.comm.eng.osaka-u.ac.jp>
2016-06-22 20:12:36 +0900

    numpy等のテスト

--- python_test/module1.py
+++ python_test/module1.py
@@ -1,2 +1,15 @@
+﻿#coding: utf-8
 from PIL import Image
+import numpy as np
+import scipy as sc
+import Cython
 
+
+def aiueo(a, b):
+    c = a + b
+    return c
+
+x=4
+y=3
+t=aiueo(x, y)
+print (t)
