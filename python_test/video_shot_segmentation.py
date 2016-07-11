4af85071697c0e29bd04be514eac3d839340a3bb
KeisukeKawamura <kawamura@nanase.comm.eng.osaka-u.ac.jp>
2016-07-11 14:35:54 +0900

    名前変更

--- python_test/video_shot_segmentation.py
+++ python_test/video_shot_segmentation.py
@@ -4,17 +4,26 @@ import scipy as sp
 
 cap = cv2.VideoCapture("5.avi")
 cv2.namedWindow("test")
-hist = sp.zeros(2)
+frame_number=1  #フレーム番号の初期化
 while cap.isOpened():
-    ret, now_frame = cap.read()
-    if ret ==False:
+    ret, now_frame = cap.read() #フレームとかを取得
+    if ret == False:    #次のフレームがなかったらbreak
         break
-    hsv = cv2.cvtColor(now_frame, cv2.COLOR_BGR2HSV)
-    hist[1], xbins, ybins = sp.histogram2d(h.ravel(),s.ravel(),[180,256],[[0,180],[0,256]])
-    hist[0]=hist[1]
-    corr = sp.corrcoef(hist[0], hist[1])
-    if corr < 0.9:
+    
+    hsv = cv2.cvtColor(now_frame, cv2.COLOR_BGR2HSV)    #フレームの色空間の変換
+    hist_now = cv2.calcHist([hsv], [0, 1], None, [180, 256], [0, 180, 0, 256])  #フレームのHSV2次元ヒストグラムを算出
 
+    if frame_number==1:#最初のフレーム用の例外処理
+        frame_number+=1
+        hist_prev = hist_now
+        continue
+    
+    corr = cv2.compareHist(hist_prev, hist_now, cv2.HISTCMP_CORREL) #ヒストグラムの比較（相関係数を利用）
+    hist_prev = hist_now    #今のフレームのヒストグラムを前のフレームのヒストグラムを格納している変数に移動
+    print(corr)
+    #  if corr < 0.9:
+    frame_number+=1
+  
     cv2.imshow("test", now_frame)
     cv2.waitKey(1)
 cap.release()
