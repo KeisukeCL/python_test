4ed6a4a5bbb94d801cb4ee244d4482c1a58fd232
KeisukeKawamura <kawamura@nanase.comm.eng.osaka-u.ac.jp>
2016-07-12 21:12:31 +0900

    video_segmentationの歓声

--- python_test/module2.py
+++ python_test/module2.py
@@ -1,3 +1,7 @@
-import KeisukeKawamura as kk
-
-kk.file_rename("G:/TH14_Test_set_mp4/TH14_test_set_mp4")
+﻿import KeisukeKawamura as kk
+import os
+import cv2
+#kk.file_rename("G:/TH14_Test_set_mp4/TH14_test_set_mp4")
+cap = cv2.VideoCapture("G:/TH14_Test_set_mp4/TH14_test_set_mp4/1.mp4")
+ret ,img = cap.read()
+print(ret)
--- python_test/video_shot_segmentation.py
+++ python_test/video_shot_segmentation.py
@@ -1,44 +1,49 @@
 ﻿import cv2
 import scipy as sp
+import os
 
-Threshold = 0.9
-cap = cv2.VideoCapture("5.avi")
-cv2.namedWindow("test")
-frame_number = 1  #フレーム番号の初期化
-shot_number = 1 #ショット番号の初期化
-while cap.isOpened():
-    ret, now_frame = cap.read() #フレームとかを取得
-    if ret == False:   #次のフレームがなかったらbreak
-        out.release()
-        break
+def video_shot_segmentation(videodetasetpath, segmentvideopath, videonumber):
+    Threshold = 0.5 #相関関数の閾値の設定
+    videopath = str(segmentvideopath)+"/" + str(videonumber) 
+    os.mkdir(videopath)   #映像ごとにディレクトリを作成
+    cap = cv2.VideoCapture(str(videodetasetpath)+"/" + str(videonumber)+".mp4") #映像の読み込み
+    frame_number = 1  #フレーム番号の初期化
+    shot_number = 1 #ショット番号の初期化
 
-    hsv = cv2.cvtColor(now_frame, cv2.COLOR_BGR2HSV)    #フレームの色空間の変換
-    hist_now = cv2.calcHist([hsv], [0, 1], None, [180, 256], [0, 180, 0, 256])  #フレームのHSV2次元ヒストグラムを算出
+    while cap.isOpened():
+        ret, now_frame = cap.read() #フレームとかを取得
 
-    if frame_number == 1:   #最初のフレーム用の例外処理
-        frame_number += 1
-        hist_prev = hist_now
-        fps = int(cap.get(cv2.CAP_PROP_FPS))
-        size = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        out = cv2.VideoWriter(str(shot_number)+".mp4", cv2.VideoWriter_fourcc("D","I","V","X"), fps, size)
-        continue
+        if ret == False:   #次のフレームがなかったらbreak
+         out.release()
+         break
+
+        hsv = cv2.cvtColor(now_frame, cv2.COLOR_BGR2HSV)    #フレームの色空間の変換
+        hist_now = cv2.calcHist([hsv], [0, 1], None, [180, 256], [0, 180, 0, 256])  #フレームのHSV2次元ヒストグラムを算出
+
+        if frame_number == 1:   #最初のフレーム用の例外処理
+            frame_number += 1
+            hist_prev = hist_now
+            fps = int(cap.get(cv2.CAP_PROP_FPS))
+            size = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            out = cv2.VideoWriter(videopath + "/" + str(shot_number)+".mp4", cv2.VideoWriter_fourcc("D","I","V","X"), fps, size)
+            continue
     
-    corr = cv2.compareHist(hist_prev, hist_now, cv2.HISTCMP_CORREL) #ヒストグラムの比較（相関係数を利用）
-    hist_prev = hist_now    #今のフレームのヒストグラムを前のフレームのヒストグラムを格納している変数に移動
-    print(corr)
-    if corr < Threshold:
-        out.release()
-        shot_number += 1
-        fps = int(cap.get(cv2.CAP_PROP_FPS))
-        size = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        out = cv2.VideoWriter(str(shot_number) + ".mp4", cv2.VideoWriter_fourcc("D", "I", "V", "X"), fps, size)
+        corr = cv2.compareHist(hist_prev, hist_now, cv2.HISTCMP_CORREL) #ヒストグラムの比較（相関係数を利用）
+        hist_prev = hist_now    #今のフレームのヒストグラムを前のフレームのヒストグラムを格納している変数に移動
+        if corr < Threshold:    #相関が閾値未満なら、映像を分割
+            out.release()
+            shot_number += 1
+            fps = int(cap.get(cv2.CAP_PROP_FPS))
+            size = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            out = cv2.VideoWriter(videopath + "/" + str(shot_number) + ".mp4", cv2.VideoWriter_fourcc("D", "I", "V", "X"), fps, size)
 
-    else:
-        out.write(now_frame)
-
-    frame_number += 1
+        else:   #相関が大きければそのまま
+            out.write(now_frame)
   
-    cv2.imshow("test", now_frame)
-    cv2.waitKey(1)
-cap.release()
-cv2.destroyWindow("test")
+    cap.release()
+ 
+
+if __name__ == '__main__':
+    for i in range(1, 10):
+        video_shot_segmentation("G:/TH14_Test_set_mp4/TH14_Test_set_mp4","G:/TH14_Test_set_mp4", i)
+        print(str(i)+ "番目の映像ショット作成")
