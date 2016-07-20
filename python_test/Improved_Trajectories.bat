f55895bc2d85f4b5b47bbbebf201a7775a9d21f0
KeisukeKawamura <kawamura@nanase.comm.eng.osaka-u.ac.jp>
2016-07-20 20:05:29 +0900

    .batファイル追加

--- python_test/Improved_Trajectories.bat
+++ python_test/Improved_Trajectories.bat
@@ -1,9 +1,9 @@
 @echo off
-
-for /L %%i in (1, 1, 1574) do (
-	mkdir G:\TH14_Test_set_mp4_Trajectories\%%i
-	for /D in ()
-	Improved_Dense_Trajectories G:\TH14_Test_set_mp4\%%i\%%i.mp4 -> G:\TH14_Test_set_mp4_Trajectories\%%i\%%i_%%i.txt
+for /L %%v in (1, 1, 3) do (
+	for %%i in (G:\TH14_Test_set_mp4\%%v\*.mp4) do (
+		echo %%i
+		Improved_Dense_Trajectories.exe %%i  -> G:\TH14_Test_set_mp4_Trajectories\%%v_%%~ni.txt
+		)
+	pause
 )
-
 echo on
