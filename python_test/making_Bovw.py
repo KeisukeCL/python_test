4a3eff0fa06163d12438f1ee84c1e758b40bbc47
KeisukeKawamura <kawamura@nanase.comm.eng.osaka-u.ac.jp>
2016-07-26 16:50:35 +0900

    実験用bovw完成

--- python_test/making_Bovw.py
+++ python_test/making_Bovw.py
@@ -8,3 +8,12 @@ with open("kmeanshog.pkl", "rb") as aa:
     codebook = pc.load(aa)
 
 bow = sp.zeros(NUMOF_K)
+with open('1.txt','r') as f:
+    d = sp.array([v.rstrip().split('\t') for v in f.readlines()])
+
+d = [list(map(lambda x:float(x), d[v])) for v in range(0,len(d))]
+for v in range(0, len(d)):
+    cls = codebook.predict(d[v][42:42+96])
+    bow[cls] += 1
+bow /= sp.linalg.norm(bow)
+print (bow)
--- python_test/making_Bovw_dictionary.py
+++ python_test/making_Bovw_dictionary.py
@@ -3,38 +3,40 @@ import scipy as sp
 from sklearn.cluster import KMeans
 import pickle as pc
 
-with open('1.txt','r') as f:
-    d = sp.array([v.rstrip().split('\t') for v in f.readlines()])
 
-d = [list(map(lambda x:float(x), d[v])) for v in range(0,len(d))]
+for i in range (1, 10, 1574):
+    with open(str(i) + '_1.txt','r') as f:
+        d = sp.array([v.rstrip().split('\t') for v in f.readlines()])
 
-#HOG 96dim: 8(bin) x 2 x 2(spatio) x 3(temporal)
-print ("HOG:")
-print (d[0][42:42+96])
+    d = [list(map(lambda x:float(x), d[v])) for v in range(0,len(d))]
 
-#HOF 108dim: 9(bin) x 2 x 2(spatio) x 3(temporal)
-print ("HOF:")
-print (d[0][138:138+108])
+    #HOG 96dim: 8(bin) x 2 x 2(spatio) x 3(temporal)
+    print ("HOG:")
+    print (d[0][42:42+96])
 
-#MBHX 96dim: 8(bin) x 2 x 2(spatio) x 3(temporal)
-print ("MBHX:")
-print (d[0][246:246+96])
+    #HOF 108dim: 9(bin) x 2 x 2(spatio) x 3(temporal)
+    print ("HOF:")
+    print (d[0][138:138+108])
 
-#MBHX 96dim: 8(bin) x 2 x 2(spatio) x 3(temporal)
-print ("MBHY:")
-print (d[0][342:342+96])
+    #MBHX 96dim: 8(bin) x 2 x 2(spatio) x 3(temporal)
+    print ("MBHX:")
+    print (d[0][246:246+96])
 
-HOGFeature = sp.vstack(d[v][42:42+96] for v in range(0, len(d)))
-#HOFFeature = sp.vstack(d[v][138:138+108] for v in range(0, len(d)))
-#MBHXFeature = sp.vstack(d[v][246:246+96] for v in range(0, len(d)))
-#MBHYFeature = sp.vstack(d[v][342:342+96] for v in range(0, len(d)))
+    #MBHY 96dim: 8(bin) x 2 x 2(spatio) x 3(temporal)
+    print ("MBHY:")
+    print (d[0][342:342+96])
 
-kmeans_hog_model = KMeans(n_clusters = 50).fit(HOGFeature)
+    HOGFeature = sp.vstack(d[v][42:42+96] for v in range(0, len(d)))
+    #HOFFeature = sp.vstack(d[v][138:138+108] for v in range(0, len(d)))
+    #MBHXFeature = sp.vstack(d[v][246:246+96] for v in range(0, len(d)))
+    #MBHYFeature = sp.vstack(d[v][342:342+96] for v in range(0, len(d)))
+    if len(HOGFeature) > 1000000:
+        break
+
+kmeans_hog_model = KMeans(n_clusters = 4000).fit(HOGFeature)
 #kmeans_hof_model = KMeans(n_clusters = 4000, max_iter = 10).fit(HOFFeature)
 #kmeans_mbhx_model = KMeans(n_clusters = 4000, max_iter = 10).fit(MBHXFeature)
 #kmeans_mbhy_model = KMeans(n_clusters = 4000, max_iter = 10).fit(MBHYFeature)
-labels = kmeans_hog_model.labels_
-for label in labels:
-    print (label)
+
 with open ("kmeanshog.pkl", "wb") as aa:
     pc.dump(kmeans_hog_model, aa)
