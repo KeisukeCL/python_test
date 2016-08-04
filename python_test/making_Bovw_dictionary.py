512f634936ba2abf45c2b27e03784dcd5fd17b01
KeisukeKawamura <kawamura@nanase.comm.eng.osaka-u.ac.jp>
2016-08-04 12:08:51 +0900

    変更

--- python_test/making_Bovw_dictionary.py
+++ python_test/making_Bovw_dictionary.py
@@ -4,8 +4,6 @@ from sklearn.cluster import KMeans
 import pickle as pc
 import gc
 
-n_clusters = 4000   #k-meansのクラスタ数の設定
-
 # Bag of Visual Wordsのコードブックを作成するコード
 for i in range (1, 51):   # ランダムにtrajectoriesをロード
     with open("G:/UCF50_Trajectories/" + str(i) + "_1.txt","r") as f:
@@ -17,47 +15,48 @@ for i in range (1, 51):   # ランダムにtrajectoriesをロード
     if i == 1:
         TRAJFeature = sp.vstack(d[v][10:10 + 32]
                                 for v in range(0, len(d)))
-        HOGFeature = sp.vstack(d[v][42:42 + 96]
-                               for v in range(0, len(d)))
-        HOFFeature = sp.vstack(d[v][138:138 + 108]
-                               for v in range(0, len(d)))
-        MBHFeature = sp.vstack(d[v][246:246 + 192]
-                               for v in range(0, len(d)))
+        #HOGFeature = sp.vstack(d[v][42:42 + 96]
+                              # for v in range(0, len(d)))
+        #HOFFeature = sp.vstack(d[v][138:138 + 108]
+                              # for v in range(0, len(d)))
+        #MBHFeature = sp.vstack(d[v][246:246 + 192]
+                              # for v in range(0, len(d)))
         print("サンプル数は"+str( len(TRAJFeature))+"個です")
     else:
         TRAJFeature2 = sp.vstack(d[v][10:10 + 32]
                                  for v in range(0, len(d)))
-        HOGFeature2 = sp.vstack(d[v][42:42 + 96]
-                                for v in range(0, len(d)))
-        HOFFeature2 = sp.vstack(d[v][138:138 + 108]
-                                for v in range(0, len(d)))
-        MBHFeature2 = sp.vstack(d[v][246:246 + 192]
-                                for v in range(0, len(d)))
+        #HOGFeature2 = sp.vstack(d[v][42:42 + 96]
+                                #for v in range(0, len(d)))
+        #HOFFeature2 = sp.vstack(d[v][138:138 + 108]
+                                #for v in range(0, len(d)))
+        #MBHFeature2 = sp.vstack(d[v][246:246 + 192]
+                                #for v in range(0, len(d)))
 
         TRAJFeature = sp.vstack((TRAJFeature, TRAJFeature2))
-        HOGFeature = sp.vstack((HOGFeature, HOGFeature2))
-        HOFFeature = sp.vstack((HOFFeature, HOFFeature2))
-        MBHFeature = sp.vstack((MBHFeature, MBHFeature2))
+        #HOGFeature = sp.vstack((HOGFeature, HOGFeature2))
+        #HOFFeature = sp.vstack((HOFFeature, HOFFeature2))
+        #MBHFeature = sp.vstack((MBHFeature, MBHFeature2))
         print("サンプル数は"+str( len(TRAJFeature))+"個です")
-        del d, TRAJFeature2, HOFFeature2, MBHFeature2
+        del d, TRAJFeature2
         gc.collect()
 
     if len(TRAJFeature) > 1000000:   # 1000000以上のサンプルを取得できたらbreak
+        print("十分なサンプル数を確保しました")
         break
 
-print("クラスタリングを実行します")
+print(str(len(TRAJFeature)) + "クラスタリングを実行します")
 # クラスタリング
-kmeans_traj_model = KMeans(n_clusters).fit(TRAJFeature)
-kmeans_hog_model = KMeans(n_clusters).fit(HOGFeature)
-kmeans_hof_model = KMeans(n_clusters).fit(HOFFeature)
-kmeans_mbh_model = KMeans(n_clusters).fit(MBHFeature)
-
+kmeans_traj_model = KMeans(n_clusters=4000, max_iter=50).fit(TRAJFeature)
+#kmeans_hog_model = KMeans(n_clusters).fit(HOGFeature)
+#kmeans_hof_model = KMeans(n_clusters).fit(HOFFeature)
+#kmeans_mbh_model = KMeans(n_clusters).fit(MBHFeature)
+print("クラスタリング終了しました")
 # pickleで保存
 with open ("kmeanstraj.pkl", "wb") as aa:
     pc.dump(kmeans_traj_model, aa)
-with open ("kmeanshog.pkl", "wb") as aa:
-    pc.dump(kmeans_hog_model, aa)
-with open ("kmeanshof.pkl", "wb") as aa:
-    pc.dump(kmeans_hof_model, aa)
-with open ("kmeansmbh.pkl", "wb") as aa:
-    pc.dump(kmeans_mbh_model, aa)
+#with open ("kmeanshog.pkl", "wb") as aa:
+    #pc.dump(kmeans_hog_model, aa)
+#with open ("kmeanshof.pkl", "wb") as aa:
+    #pc.dump(kmeans_hof_model, aa)
+#with open ("kmeansmbh.pkl", "wb") as aa:
+    #pc.dump(kmeans_mbh_model, aa)
